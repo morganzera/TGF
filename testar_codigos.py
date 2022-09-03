518f5eac9eae92b4f4186d0e0eda91e167ac0802
Morgan <180075845@aluno.unb.br>
2022-09-03 17:49:27 -0300

    comecei a parte de correlacao cruzada. Para realizar essa correlacao ainda nao sei se utiliza as freq da fft ou da wavelet.

--- testar_codigos.py
+++ testar_codigos.py
@@ -1,12 +1,2 @@
-import pandas as pd
-# List of Tuples
-fruit_list = [ ('Orange', 34, 'Yes' )]
-#Create a DataFrame object
-df = pd.DataFrame(fruit_list, columns = ['Name' , 'Price', 'Stock'])
-#Add new ROW
-df=df.append({'Name' : 'Apple' , 'Price' : 23, 'Stock' : 'No'} , ignore_index=True)
-df=df.append({'Name' : 'Mango' , 'Price' : 13, 'Stock' : 'Yes'} , ignore_index=True)
-print(df)
-
-def adicionar0(original, mais):
-    referencia = original.append(mais)
+a = 2+3j
+print(type(a))
